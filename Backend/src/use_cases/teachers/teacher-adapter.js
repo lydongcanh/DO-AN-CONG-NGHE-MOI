bcfe2b983bd5371376d78969820fe2d62fde20f2
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-01-01 07:25:49 +0700

    [Backend] Update teacher use-cases, remove find-teacher-schedules.

--- Backend/src/use_cases/teachers/teacher-adapter.js
+++ Backend/src/use_cases/teachers/teacher-adapter.js
@@ -1,12 +1,10 @@
 import CreateTeacher from "./create-teacher";
-import FindTeacherSchedules from "./find-teacher-schedules";
 import FindTeacherWithAccount from "./find-teacher-with-account";
 
 export default class TeacherAdapter {
     
     constructor(tableName, region, endpoint) {
         this.createTeacherObj = new CreateTeacher(tableName, region, endpoint);
-        this.findTeacherSchedulesObj = new FindTeacherSchedules(tableName, region, endpoint);
         this.findTeacherWithAccountObj = new FindTeacherWithAccount(tableName, region, endpoint);
     }
 
@@ -16,14 +14,6 @@ export default class TeacherAdapter {
      */
     async createTeacher(teacher) {
         return await this.createTeacherObj.execute(teacher);
-    }
-
-    /**
-     * Find all schedules of a teacher with provided id.
-     * @param {String} teacherId 
-     */
-    async findTeacherSchedules(teacherId) {
-        return await this.findTeacherSchedulesObj.execute(teacherId);
     }
 
     /**
