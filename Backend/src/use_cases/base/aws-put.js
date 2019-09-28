ae830aa06286aa1d4f2ec5c32a8577fa0842e66d
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-09-28 19:12:15 +0700

    [Backend] Rename aws-create to aws-put.

---
+++ Backend/src/use_cases/base/aws-put.js
@@ -0,0 +1,26 @@
+import AWS from "aws-sdk";
+import AWSUseCase from "./aws-use-case";
+
+export default class AWSPut extends AWSUseCase {
+    constructor(tableName, region, endpoint) {
+        super(tableName, region, endpoint);
+        this.docClient = new AWS.DynamoDB.DocumentClient();
+    }
+
+    /**
+     * Override this method to provide "create" params.
+     */
+    get params() { 
+        console.log("This method does nothing. Please override it to provide \"create\" params.");
+    }
+    
+    async execute() {
+        this.docClient.put(this.params, (error, data) => {
+            if (error) {
+                console.log("Unable to add item. Error: " + JSON.stringify(error));
+            } else {
+                console.log("Added item: " + JSON.stringify(data));
+            }
+        });
+    }
+}
