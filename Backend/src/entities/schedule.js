8b6f92ab9724b1fa3c7c889dd309e5f97631eab1
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-06 23:11:38 +0700

    [Backend] Move subject prop from teacher to schedule.

--- Backend/src/entities/schedule.js
+++ Backend/src/entities/schedule.js
@@ -6,13 +6,15 @@ export default class Schedule {
      * @param {String} state 
      * @param {Studyclass} studyclass 
      * @param {Teacher} teacher 
+     * @param {Subject} subject
      */
-    constructor(id, time, state, studyclass, teacher) {
+    constructor(id, time, state, studyclass, teacher, subject) {
         this.id = id;
         this.time = time;
         this.state = state;
         this.studyclass = studyclass;
         this.teacher = teacher;
+        this.subject = subject;
         Object.freeze(this);
     }
 }
--- Backend/src/entities/teacher.js
+++ Backend/src/entities/teacher.js
@@ -9,10 +9,9 @@ export default class Teacher {
      * @param {String} email 
      * @param {String} phoneNumber 
      * @param {String} state 
-     * @param {Subject} subject 
      * @param {Account} account 
      */
-    constructor(id, name, gender, birthday, address, email, phoneNumber, state, subject, account) {
+    constructor(id, name, gender, birthday, address, email, phoneNumber, state, account) {
         this.id = id;
         this.name = name;
         this.gender = gender;
@@ -21,7 +20,6 @@ export default class Teacher {
         this.email = email;
         this.phoneNumber = phoneNumber;
         this.state = state;
-        this.subject = subject;
         this.account = account;
         Object.freeze(this);
     }
