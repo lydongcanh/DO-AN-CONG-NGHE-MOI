205a9bcc113af0d2791dfc15ad7904843672e96f
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-06 23:06:14 +0700

    [Backend] Remove student prop from scoreboard.

--- Backend/src/entities/scoreboard.js
+++ Backend/src/entities/scoreboard.js
@@ -1,19 +1,16 @@
 export default class Scoreboard {
     
     /**
-     * 
      * @param {String} id 
      * @param {String} semester 
      * @param {String} year 
      * @param {Array<Score>} scores 
-     * @param {Student} student 
      */
-    constructor(id, semester, year, scores, student) {
+    constructor(id, semester, year, scores) {
         this.id = id;
         this.semester = semester;
         this.year = year;
         this.scores = scores;
-        this.student = student;
         Object.freeze(this);
     }
 }
