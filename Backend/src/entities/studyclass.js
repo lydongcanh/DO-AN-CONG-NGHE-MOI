f78c34e1c1e81f980e0bf53a9097449044a8351c
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-06 23:06:50 +0700

    [Backend] Remove homeroomTeacher prop from studyclass.

--- Backend/src/entities/studyclass.js
+++ Backend/src/entities/studyclass.js
@@ -4,15 +4,13 @@ export default class Studyclass {
      * @param {String} id 
      * @param {String} name 
      * @param {String} grade
-     * @param {Teacher} homeroomTeacher 
      * @param {String} state 
      * @param {Array<Student>} students 
      */
-    constructor(id, name, grade, homeroomTeacher, state, students) {
+    constructor(id, name, grade, state, students) {
         this.id = id;
         this.name = name;
         this.grade = grade;
-        this.homeroomTeacher = homeroomTeacher;
         this.state = state;
         this.students = students;
         Object.freeze(this);
