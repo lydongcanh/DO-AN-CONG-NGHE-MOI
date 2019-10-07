817d058cd711581bf696fe8f0e67248f987285d1
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-07 23:04:53 +0700

    [Backend] Update aws-use-case, only set region & endpoint in constructor.

--- Backend/src/repositories/aws/aws-use-case.js
+++ Backend/src/repositories/aws/aws-use-case.js
@@ -25,32 +25,8 @@ export default class AWSUseCase {
         return this._region;
     }
 
-    set region (region) {
-        this.updateConfiguration(this.tableName, region, this._endpoint);
-    }
-
     get endpoint() {
         return this._endpoint;
-    }
-
-    set endpoint(endpoint) {
-        this.updateConfiguration(this.tableName, this._region, endpoint);
-    }
-
-    /**
-     * @param {String} tableName
-     * @param {String} region Region. Use "local" as default. Note that only "us-east-1" is available for AWS Student account.
-     * @param {String} endpoint
-     */
-    updateConfiguration(tableName, region, endpoint) {
-        this.tableName = tableName;
-        this._region = region;
-        this._endpoint = endpoint;
-
-        AWS.config.update({
-            region: this._region,
-            endpoint: this._endpoint
-        });
     }
 
     /**
