867f7a20f520231389c44fd251a217db02fe9cc7
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-10 03:38:13 +0700

    [Backend] Add missing schedule router.

--- Backend/src/routes/scheduleRouter.js
+++ Backend/src/routes/scheduleRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import uuidvi from "uuid/v1";
+import uuidv1 from "uuid/v1";
 import { TABLE_NAME, REGION, ENDPOINT } from "../dynamodb-config";
 import Schedule from "../entities/schedule";
 import ScheduleAdapter from "../use_cases/schedules/schedule-adapter";
@@ -7,23 +7,31 @@ import ScheduleAdapter from "../use_cases/schedules/schedule-adapter";
 const router = express.Router();
 const scheduleAdapter = new ScheduleAdapter(TABLE_NAME, REGION, ENDPOINT);
 
-router.post("/create", async (request, response, _) => {
+router.get("/", async (request, response, _) => {
+    const {teacherId, studyclassId} = request.query;
+    if (teacherId) {
+        const result = await scheduleAdapter.findTeacherSchedules(teacherId);
+        response.send(result);
+    } else if (studyclassId) {
+        const result = await scheduleAdapter.findStudyclassSchedules(studyclassId);
+        response.send(result);
+    } else {
+        // TODO: send all available schedules instead.
+        response.send({
+            isSuccess: false,
+            error: {
+                message: "un-supported query",
+                statusCode: 405
+            }
+        });
+    }
+});
+
+router.post("/", async (request, response, _) => {
     const {time, classId, teacherId, subjectId} = request.body;
-    const id = uuidvi();
+    const id = uuidv1();
     const schedule = new Schedule(id, time, state, classId, teacherId, subjectId);
     const result = await scheduleAdapter.createSchedule(schedule);
-    response.send(result);
-});
-
-router.get("/teacher/:teacherId", async (request, response, _) => {
-    const teacherId = request.params.teacherId;
-    const result = await scheduleAdapter.findTeacherSchedules(teacherId);
-    response.send(result);
-});
-
-router.get("/student/:studyclassId", async (request, response, _) => {
-    const studyclassId = request.params.studyclassId;
-    const result = await scheduleAdapter.findStudyclassSchedules(studyclassId);
     response.send(result);
 });
 
