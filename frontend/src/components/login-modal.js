5064f692c39d5c68cd607e166d26d9e13a2cfea7
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-23 23:15:41 +0700

    [Frontend] Fix minus issue and refactor login modal.

--- frontend/src/components/login-modal.js
+++ frontend/src/components/login-modal.js
@@ -2,39 +2,56 @@ import React, { Component } from "react";
 import { Button, Form, Input, Modal } from "antd";
 
 export default class LoginModal extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
-        username : "",
-        password : "",
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            username: "",
+            password: "",
+        }
+
+        this.handleLoginButton = this.handleLoginButton.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-    this.handleLoginButton = this.handleLoginButton.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  
-  render() {
-    return (
-      <Modal visible={this.props.visible} onCancel={this.props.handleOk} title="Đăng nhập" footer={[null]} style={{textAlign:"center"}}>
-        <Form className="login-form" >
-          <Form.Item><Input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleChange} /></Form.Item>
-          <Form.Item><Input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleChange} /></Form.Item>
-          <Form.Item><Button type="primary" htmlType="submit" onClick={this.handleLoginButton} className="login-form-button">Đăng nhập</Button></Form.Item>
-        </Form>
+
+    render() {
+        return (
+            <Modal visible={this.props.visible} 
+                   onCancel={this.props.handleOk} 
+                   title="Đăng nhập" 
+                   footer={[null]} 
+                   style={{ textAlign: "center" }}>
+            <Form className="login-form" >
+                <Form.Item>
+                    <Input type="text" name="username" placeholder="Tài khoản" value={this.state.username} onChange={this.handleChange} />
+                </Form.Item>
+                <Form.Item>
+                    <Input type="password" name="password" placeholder="Mật khẩu" value={this.state.password} onChange={this.handleChange} />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" onClick={this.handleLoginButton} style={{width: "100%"}}>
+                        Đăng nhập
+                    </Button>
+                </Form.Item>
+            </Form>
       </Modal>
     );
   }
-  handleChange(e) {
-    //gan gia tri khi nhap
-    this.setState({[e.target.name]: e.target.value});
- }
-  handleLoginButton(e) {
-    // log username, password
-    alert(`username : ${this.state.username} password : ${this.state.password}`);
-    // Ẩn modal
-    this.props.handleOk(e);
-  }
-  
+    handleChange(e) {
+        //gan gia tri khi nhap
+        this.setState({ 
+            [e.target.name]: e.target.value 
+        });
+    }
+    
+    handleLoginButton(e) {
+        // log username, password
+        alert(`username : ${this.state.username} password : ${this.state.password}`);
+        // Ẩn modal
+        this.props.handleOk(e);
+    }
 }
+
 // viet ben trang goi modal
 //   constructor(props){
 //     super(props);
@@ -43,14 +60,14 @@ export default class LoginModal extends Component {
 //     }
 //   }
 //   render() { 
-    
+
 //     return (
 //       <BrowserRouter>
 //         <Button type="primary"  onClick={this.handleOk}>
 //           Đăng nhập
 //         </Button>
 //         <LoginModal handleOk={this.handleCancel} visible={this.state.visible}>
-          
+
 //         </LoginModal>
 //       </BrowserRouter>
 //     );
