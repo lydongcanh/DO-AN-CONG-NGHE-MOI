9f36962909d6f068157d248c9d7a592991293bb1
Ky Thanh <kykikwang@gmail.com>
2019-10-24 00:13:01 +0700

    [Frontend] Add Component Student

---
+++ frontend/src/components/student/student-form.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Form,Button} from 'antd';
+
+export default class UserDetails extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+
+    }
+  }
+  
+  render(){
+    return(
+        <div>
+            <Form>
+                <Form.Item>
+                    
+                </Form.Item>
+                <Form.Item>
+                    
+                </Form.Item>
+                <Form.Item>
+                    
+                </Form.Item>
+            </Form>
+        </div>
+    )
+  }
+ 
+}
+ 
