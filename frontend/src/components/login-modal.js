c5e35c9fc46d0fb6487a2cf31ae3c8cae34b6a1f
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-25 05:51:51 +0700

    [Frontend] Update login modal.

--- frontend/src/components/login-modal.js
+++ frontend/src/components/login-modal.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Form, Input, Modal } from "antd";
+import { Button, Form, Input, Modal, Icon } from "antd";
 
 import MockDB from "../repository/mock/mockDB";
 const mockDB = new MockDB();
@@ -27,18 +27,31 @@ export default class LoginModal extends Component {
                 visible={this.props.visible}
                 onCancel={this.props.handleCancel}
                 title="Đăng nhập"
-                closable={false}
                 footer={null}
+                width="30%"
                 style={{ textAlign: "center" }}>
                 <Form>
                     <Form.Item>
-                        <Input type="text" name="username" placeholder="Tài khoản" value={this.state.username} onChange={this.handleChange} />
+                        <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                               type="text" 
+                               name="username" 
+                               placeholder="Tài khoản" 
+                               value={this.state.username} 
+                               onChange={this.handleChange} />
                     </Form.Item>
                     <Form.Item>
-                        <Input type="password" name="password" placeholder="Mật khẩu" value={this.state.password} onChange={this.handleChange} />
+                        <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                               type="password" 
+                               name="password" 
+                               placeholder="Mật khẩu" 
+                               value={this.state.password} 
+                               onChange={this.handleChange} />
                     </Form.Item>
                     <Form.Item>
-                        <Button type="primary" htmlType="submit" onClick={this.handleLoginButton} style={{ width: "100%" }}>
+                        <Button type="primary" 
+                                htmlType="submit" 
+                                onClick={this.handleLoginButton} 
+                                style={{ width: "100%" }}>
                             Đăng nhập
                         </Button>
                     </Form.Item>
