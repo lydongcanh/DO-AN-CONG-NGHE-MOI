c871a17fe0a896a959cdb304934b25f4562743b8
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-26 08:10:05 +0700

    [Backend] Submit scoreboard changes.

--- Backend/src/use_cases/scoreboards/scoreboard-adapter.js
+++ Backend/src/use_cases/scoreboards/scoreboard-adapter.js
@@ -1,12 +1,10 @@
 import CreateScoreBoard from "./create-scoreboard";
-import FindScoreBoardByStudentID from "./find-scoreboard-by-studentid";
 import GetAllScoreboards from "./get-all-scoreboards";
 import GetScoreboardById from "./get-scoreboard-by-id";
 
 export default class ScoreboardAdapter {
     constructor(tablename,region,endpoint) {
         this._createScoreBoardObj = new CreateScoreBoard(tablename, region, endpoint);
-        this._findScoreBoardByStudentIDObj = new FindScoreBoardByStudentID(tablename, region, endpoint);
         this._getAllScoreboardsObj = new GetAllScoreboards(tablename, region, endpoint);
         this._getScoreboardByIdObj = new GetScoreboardById(tablename, region, endpoint);
     }
@@ -18,15 +16,7 @@ export default class ScoreboardAdapter {
     async createScoreBoard(scoreboard){
         return await this._createScoreBoardObj.execute(scoreboard);
     }
-
-    /**
-     * Find scoreboard with provided student id.
-     * @param {String} studentId 
-     */
-    async findScoreboardByStudentId(studentId){
-        return await this._findScoreBoardByStudentIDObj.execute(studentId);
-    }
-
+    
     async getAllScoreboards() {
         return await this._getAllScoreboardsObj.execute();
     }
