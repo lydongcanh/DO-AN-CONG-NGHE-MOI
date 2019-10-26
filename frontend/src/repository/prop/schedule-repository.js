3f8ba16279828baecf535c356b904971dfd8e812
Lý Đông Cảnh <lydongcanh@gmail.com>
2019-10-26 10:10:38 +0700

    [Backend] Add real repository.

---
+++ frontend/src/repository/prop/schedule-repository.js
@@ -0,0 +1,78 @@
+import axios from "axios";
+import { schedulesEndpoint } from "./endpoints";
+
+class ScheduleRepository {
+    async createSchedule(classId, teacherId, startDate, endDate, startTime, length, state, subject) {
+        try {
+            const schedule = {
+                classId: classId,
+                teacherId: teacherId,
+                startDate: startDate,
+                endDate: endDate,
+                startTime: startTime,
+                length: length,
+                state: state,
+                subject: subject
+            };
+
+            const result = await axios.post(schedulesEndpoint, schedule);
+            return result.data.success ? schedule : { error: result.data.error };
+        } catch (error) {
+            return {error: error};
+        }
+    }
+
+    async getAllSchedules() {
+        try {
+            const result = await axios.get(schedulesEndpoint);
+            return result.data.success ? result.data.body.Items : { error: result.data.error };
+        } catch (error) {
+            return { error: error };
+        }
+    }
+
+    async getScheduleById(id) {
+        try {
+            const result = await axios.get(`${schedulesEndpoint}/${id}`);
+            if (!result.data.success)
+                return result.data.error;
+
+            if (result.data.body.Items.length < 1)
+                return { error: "NotFound" };
+
+            const schedule = result.data.body.Items[0];
+            return {
+                id: schedule.sortKey,
+                classId: schedule.data,
+                teacherId: schedule.teacherId,
+                startDate: schedule.startDate,
+                endDate: schedule.endDate,
+                startTime: schedule.startTime,
+                length: schedule.length,
+                state: schedule.state,
+                subject: schedule.subject
+            };
+        } catch (error) {
+            return {error: error};
+        }
+    }
+
+    async getSchedulesByClassId(classId) {
+        const result = await this.getAllSchedules();
+        if (result.error)
+            return result;
+
+        return result.filter(schedule => schedule.classId == classId);
+    }
+
+    async getSchedulesByTeacherId(teacherId) {
+        const result = await this.getAllSchedules();
+        if (result.error)
+            return result;
+        
+        return result.filter(schedule => schedule.teacherId == teacherId);
+    }
+}
+
+const scheduleRepository = new ScheduleRepository();
+export default scheduleRepository;
---
+++ frontend/src/repository/prop/teacher-repository.js
@@ -0,0 +1,67 @@
+import axios from "axios";
+import {teachersEndpoint} from "./endpoints";
+
+class TeacherRepository {
+    async createTeacher(name, gender, birthday, address, email, phoneNumber, state) {
+        try {
+            const teacher = {
+                name: name,
+                gender: gender,
+                birthday: birthday,
+                address: address,
+                phoneNumber: phoneNumber,
+                email: email,
+                state: state,
+            };
+
+            const result = axios.post(teachersEndpoint, teacher);
+            return result.data.success ? teacher : { error: result.data.error };
+        } catch (error) {
+            return { error: error };
+        }
+    }
+
+    async getAllTeachers() {
+        try {
+            const result = await axios.get(teachersEndpoint);
+            return result.data.success ? result.data.body.Items : { error: result.data.error }
+        } catch (error) {
+            return { error: error };
+        }
+    }
+
+    async getTeacherById(id) {
+        try {
+            const result = await axios.get(`${teachersEndpoint}/${id}`);
+            if (!result.data.success)
+                return result.data.error;
+
+            if (result.data.body.Items.length < 1)
+                return { error: "NotFound" };
+
+            const teacher = result.data.body.Items[0];
+            return {
+                id: teacher.sortKey,
+                name: teacher.data,
+                gender: teacher.gender,
+                birthday: teacher.birthday,
+                address: teacher.address,
+                phoneNumber: teacher.phoneNumber,
+                state: teacher.state
+            };
+        } catch (error) {
+            return { error: error };
+        }
+    }
+
+    async getTeachersByName(name) {
+        const result = await this.getAllTeachers();
+        if (result.error)
+            return result;
+
+        return result.filter(teacher => teacher.name.include(name));
+    }
+}
+
+const teacherRepository = new TeacherRepository();
+export default teacherRepository;
