94c3b80d340875e0f33c0b55cd94728e394ab689
Ky Thanh <kykikwang@gmail.com>
2019-11-01 11:38:11 +0700

    [Backend] Update aws config.

--- Backend/src/repositories/aws/aws-use-case.js
+++ Backend/src/repositories/aws/aws-use-case.js
@@ -12,7 +12,10 @@ export default class AWSUseCase {
      */
     constructor(tableName, region, endpoint) {
         let updateConfig = {
-            region: region
+            region: region,
+            accessKey: "ASIA3GHZ2RT64L5W2QUS",
+            secretAccessKey: "aws_secret_access_key=rh3DAGDuxIEIvSf3u54teUBb3hPbbNysJYlsQazf",
+            sessionToken: "FQoGZXIvYXdzEO3//////////wEaDFqa1h46KK5HDGDIxCKDAqlmifbFdOLqUCAHCJpEDKwJUh7f9IrdnUlYCkSgI+RUlEPJ5Q2E8nQGy1GUzbRx36+fK5jZF85H3j5NcR+1FDFfYEu/uhapohQdDvGMx+1YcOqAKX34QWJvVE6mB0TSBTzz+nXwTAaU1pMVYApu+03Ry+U6Xl3wpSmesFqWVA3hAxIZnlhKGixWmTgPBCxJimqkoWI/+bZ5hd6gJQakorsjrlRLs6/mtEV3UKKDg251Co+mS31RBZtUF7WPEFlZbDgQlfd/iUVtDCGrbbK0Lo7tNohXIWsqryc44KtasCsjUomxiSsyKxg3i5u0rimcepeH8utchTCiynuI/FyTzM+1WfAo2Mvu7QU="
         };
 
         if (endpoint)
