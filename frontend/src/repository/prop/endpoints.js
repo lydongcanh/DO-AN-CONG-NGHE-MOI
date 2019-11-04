133417af7e1a886a9b543b95508a224ae387a1a8
Ky Thanh <kykikwang@gmail.com>
2019-11-04 22:40:16 +0700

    [Frontend[ Edit endpoint

--- frontend/src/repository/prop/endpoints.js
+++ frontend/src/repository/prop/endpoints.js
@@ -1,4 +1,4 @@
-export const baseEndpoint = "ec2-18-209-173-87.compute-1.amazonaws.com";
+export const baseEndpoint = "ec2-18-209-173-87.compute-1.amazonaws.com:8000";
 export const accountsEndpoint = baseEndpoint + "/accounts";
 export const schedulesEndpoint = baseEndpoint + "/schedules";
 export const scoresEndpoint = baseEndpoint + "/scores";
